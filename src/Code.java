34eb08e0561555cca45b052d7aff8ae67ffea50a
Yury.Fedoseev <Yri95@inbox.ru>
2021-07-01 13:33:38 +0300

    Добавление кода и основных файлов

---
+++ src/Code.java
@@ -0,0 +1,8 @@
+public class Code {
+    public static void main(String [] args){
+        int balanse= 2_000_000_000; //Баланс на счете/карте
+        int perevod= 500_000_000; //Сумма перевода/пополнения
+        int total = balanse  + perevod;
+        System.out.println(total);
+    }
+}
